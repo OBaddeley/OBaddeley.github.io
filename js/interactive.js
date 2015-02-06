04bd3ad15ee11890f1bb2c0ba9dbcc5309852e15
OliviaHihi <Olivia.Hihi@users.noreply.github.com>
2015-02-07 12:00:31 +1300

    Add interactive overlay to images on home page

---
+++ js/interactive.js
@@ -0,0 +1,28 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+
+$overlay.append($image);
+
+$overlay.append($caption);
+
+$("body").append($overlay);
+
+//Capture the click event on a link to an image
+$("#gallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+
+  $image.attr("src", imageLocation);
+  
+  $overlay.show();
+ 
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+
+$overlay.click(function(){
+  $overlay.hide();
+});
